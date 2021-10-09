77f0a4b0bc6de6b7e00682539a44940141edf251
Mr.Ben <42569624+1205BEN@users.noreply.github.com>
2021-10-09 16:17:50 +0700

    Final Project

---
+++ src/dist/data.dev.js
@@ -0,0 +1,32 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports.products = void 0;
+var products = [{
+  id: 1,
+  img: "https://images.unsplash.com/photo-1546587348-d12660c30c50?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  link: "http://lama.dev"
+}, {
+  id: 2,
+  img: "https://images.unsplash.com/photo-1543877087-ebf71fde2be1?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8bmF0dXJhbHxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  link: "http://lama.dev"
+}, {
+  id: 3,
+  img: "https://media.istockphoto.com/photos/put-more-in-get-more-out-picture-id1291318636?b=1&k=20&m=1291318636&s=170667a&w=0&h=UvVIk7wwkN3X9OFm8gBlWWviV5vAjfrq2ejYP30JmnA=",
+  link: "http://lama.dev"
+}, {
+  id: 4,
+  img: "https://media.istockphoto.com/photos/sporty-man-on-the-mountain-peak-looking-on-mountain-valley-with-at-picture-id1277015766?b=1&k=20&m=1277015766&s=170667a&w=0&h=3vkjdvbCKE6q1ixhl_u190XOTqZYVvpGSpngOKdEZZ0=",
+  link: "http://lama.dev"
+}, {
+  id: 5,
+  img: "https://images.unsplash.com/photo-1489533119213-66a5cd877091?ixid=MnwxMjA3fDB8MHxzZWFyY2h8OXx8bGlmZXxlbnwwfHwwfHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  link: "http://lama.dev"
+}, {
+  id: 6,
+  img: "https://media.istockphoto.com/photos/humans-hands-calculating-money-for-life-planning-picture-id1290658036?b=1&k=20&m=1290658036&s=170667a&w=0&h=IsiKD_HMonBJaQwmUKlcIUcUpFzw88vwiDm0v8yrVeA=",
+  link: "http://lama.dev"
+}];
+exports.products = products;
